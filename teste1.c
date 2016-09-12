2170c5d3dd4c5432d82e898c6444567e273f35a1
duarte062000 <dmpc2000@gmail.com>
2016-09-12 16:05:47 +0100

    Update teste1.c

--- teste1.c
+++ teste1.c
@@ -3,10 +3,15 @@
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
@@ -14,6 +19,11 @@ void loop() {
   digitalWrite(3, HIGH);
   delay(100);
   digitalWrite(3, LOW);
+  delay(100);
+
+  digitalWrite(4, HIGH);
+  delay(100);
+  digitalWrite(4, LOW);
   delay(100);
 
   digitalWrite(5, HIGH);
@@ -26,14 +36,39 @@ void loop() {
   digitalWrite(6, LOW);
   delay(100);
 
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+  delay(100)
+  
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  
   digitalWrite(9, HIGH);
   delay(100);
   digitalWrite(9, LOW);
   delay(100);
-
+  
   digitalWrite(10, HIGH);
   delay(100);
   digitalWrite(10, LOW);
+  delay(100);
+  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW);
+  delay(100);
+  
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);
+  delay(100);
+  
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
   delay(100);
 
 }
